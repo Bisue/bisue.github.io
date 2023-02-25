625a126c844ac062bf6acc5744f17ab197f68168
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-02-26 00:35:15 +0900

    폰트 적용 (default, fancy, code)

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,6 +7,36 @@ export default defineUserConfig({
 
   lang: "ko-KR",
 
+  head: [
+    // Link google fonts - korean supported fonts
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding:wght@400;700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
+
   theme,
 
   // Enable it with pwa
