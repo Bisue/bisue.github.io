5457ef604eab2555ec58dd5a3a163eb1e096f533
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-03-13 04:07:54 +0900

    feat: YoutubePlayer 컴포넌트 추가
    [TODO]
    - Youtube Player API instead of iframe

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,8 +1,10 @@
 import { defineClientConfig } from "@vuepress/client";
 import TextHighlight from "./components/TextHighlight.vue";
+import YoutubePlayer from "./components/YoutubePlayer.vue";
 
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
     app.component("TextHighlight", TextHighlight);
+    app.component("YoutubePlayer", YoutubePlayer);
   },
 });
---
+++ src/.vuepress/components/YoutubePlayer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="youtube-player-container">
+    <iframe
+      class="youtube-player"
+      :src="`https://www.youtube-nocookie.com/embed/${props.id}?modestbranding=1`"
+      :title="props.title"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+/**
+ * [TODO] iframe 대신 Youtube Player API 사용하기
+ * 참고: https://developers.google.com/youtube/iframe_api_reference?hl=ko
+ */
+const props = defineProps<{
+  id: string;
+  title: string;
+}>();
+</script>
+
+<style>
+.youtube-player {
+  aspect-ratio: 16 / 9;
+  width: 100%;
+  max-width: 100%;
+}
+</style>
